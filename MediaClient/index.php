0924b2d8a5bc4d02f6b921c57f567696e207d7da
Matrix Shadow <M4trixSh4d0w@gmail.com>
2015-11-25 22:08:04 -0700

    Created basic folder finder

--- MediaClient/index.php
+++ MediaClient/index.php
@@ -20,12 +20,20 @@
   $current_dir = $_GET["dir"];
   if(!isset($_GET["dir"])) {
 	  $current_dir = "media";
+  } else {
+	  $current_dir = realpath($current_dir);
   }
   
+  	//Function found at:
+	//http://stackoverflow.com/questions/834303/startswith-and-endswith-functions-in-php
+	function endsWith($haystack, $needle) {
+		// search forward starting from end minus needle length characters
+		return $needle === "" || (($temp = strlen($haystack) - strlen($needle)) >= 0 && strpos($haystack, $needle, $temp) !== FALSE);
+	}
   
-  function listAllDirs($dir, $path) {
-	$path = $dir;
-	$items = scandir($dir);
+	function listAllDirs($dir, $path) {
+		$path = $dir;
+		$items = scandir($dir);
 		foreach($items as &$item) {
 			if($item != "." && $item != "..") {
 				$file = $path . "/" . $item;
@@ -37,53 +45,126 @@
 				}	
 			}
 		}
-  }
+	}
+
+	function readExtensionFile($file) {
+		$lines = file($file);
+		$array = array("None");
+		$count = 0;
+		foreach($lines as $line_num => $line) {
+			$array[$count] = $line;
+			$count++;
+		}
+		return $array;
+	}
   
-  echo "<div align=\"center\">";
-  echo "<div style=\"height:60vh;width:100vh;border:1px solid #ccc;overflow:auto;\">";
+	$picture_extensions = readExtensionFile("media/Pictures/extensions.txt");
+	$movie_extensions = readExtensionFile("media/Movies/extensions.txt");
+	$music_extensions = readExtensionFile("media/Music/extensions.txt");
+	$game_extensions = readExtensionFile("media/Games/extensions.txt");
+	$doc_extensions = readExtensionFile("media/Documents/extensions.txt");
+	
+	
+	echo "<div align=\"center\">";
+	echo "<div style=\"height:60vh;width:100vh;border:1px solid #ccc;overflow:auto;\">";
 
   
-  echo "<table>";
-  echo "<tr>";
+	echo "<table border=1>";
+	echo "<tr>";
+	
+	
 
   
   $dir = $current_dir;
   $rowcount = 0;
   $rowmax = 5;
-  $colcount = 0;
-  $media = scandir($dir);
+  $media = scandir(realpath($dir)) or die("Unable to read requested directory!");
   
   foreach($media as &$item) {
-	  if($item != ".." && $item != ".") {
+	  if($item != ".") {
 		 $file = $dir . "/" . $item;
 		 
 		if($rowcount < $rowmax) {
-			echo "<td>";
-			$image = "";
+			$type = "";
 			$height = 0;
 			if(is_dir($file)) {
-				$image = "folder.png";
-				
+				$type = "folder";
 			} else {
+				$found = false;
+				foreach($picture_extensions as $ext) {
+					if(endsWith($item, $ext)) {
+							$type = "picture"; 
+							$found = true;
+					}
+				}
 				
-				$image = "text.png";
+				if(!$found) {
+					foreach($movie_extensions as $ext) {
+						if(endsWith($item, $ext)) {
+								$type = "video"; 
+								$found = true;
+						}
+					}
+				}
 				
+				if(!$found){
+					foreach($music_extensions as $ext) {
+						if(endsWith($item, $ext)) {
+								$type = "music"; 
+								$found = true;
+						}
+					}
+				}
+				
+				if(!$found) {
+					foreach($game_extensions as $ext) {
+						if(endsWith($item, $ext)) {
+								$type = "zipfile"; 
+								$found = true;
+						}
+					}
+				}
+				
+				if(!$found) {
+					foreach($doc_extensions as $ext) {
+						if(endsWith($item, $ext)) {
+							$type = "text";
+							$found = true;
+						}
+					}
+				}
+				
+				if(!$found) {
+					$type = "unknown";
+					$found = true;
+				}
+				
+				
+				}
+			
+
+			list($width, $height, $ttype, $attr) = getimagesize("res/" . $type . ".png");
+			
+			echo "<td style=\"word-break:break-all;\">";
+			if(!is_dir($file)) {
+				echo "<a href=\"" . $type . ".php?file=" . $item . "\">";
+			} else {
+				echo "<a href=\"index.php?dir=" . $dir . "/" . $item . "\">";
 			}
-			list($width, $height, $type, $attr) = getimagesize("res/" . $image);
-			echo "<a href=\"index.php?dir=" . $dir . "/" . $item . "\">";
-			echo "<div align=\"center\"><p style=\"position:relative;top:" . ($height/2 - 20) .";color:#000;\">" . $item ."</p></div>";
-			echo "<img src=\"res/" . $image . "\" alt=\"" . $item . "\" width=\"". $width/2 . "\">";
+			
+			echo "<div align=\"center\"><img src=\"res/" . $type . ".png\" alt=\"" . $item . "\" width=\"". $width/2 . "\"></div>";
+			echo "<div align=\"center\"><p style=\"color:#000;\">" . $item ."</p></div>";
 			echo "</a>";
 			echo "</td>";
 			
 			$rowcount++;
-		} else {
-				echo "</tr><tr>";
-				$rowcount = 0;
-				$colcount++;
+			} else {
+					echo "</tr>\n<tr>";
+					$rowcount = 0;
+			}
 		}
 	}
-}
+	echo "</tr>";
 	echo "</table>";
   //listAllDirs($dir, "");
 	echo "</div>";
